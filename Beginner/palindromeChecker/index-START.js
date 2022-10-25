c5efcafcf4ef8eca7c7556184ccb9e0515804209
Devin Lane <devin.c.lane@gmail.com>
2022-10-25 13:06:24 -0700

    palindrome checker

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -7,7 +7,22 @@ e.g palindromeChecker('racecar') // will return true
 
 
 function palindromeChecker(text) {
-    // Code goes here
+    const reversedText = text.split('').reverse().join('')
+    // is the reversed word the same as the original word?
+    // if so then it's a palindrome and we return true, otherwise false
+    return reversedText === text
+}
+
+function palindromeChecker(text) {
+    // go across half the text
+    for (let i = 0; i < text.length / 2; i++) {
+        // if one letter ever does not equal another letter in the spot it would be if reversed, return false
+        // e.g., "applesauce", "a" should equal "e" i.e., text[0] should equal text.length - 1 - the index we are at.
+        if (text[i] !== text[text.length - 1 - i]) {
+            return false
+        }
+    }
+    return true;
 }
 
 
