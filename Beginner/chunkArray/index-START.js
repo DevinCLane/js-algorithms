8a115d991cbcee3be62bb58c42eeb0249996e935
Devin Lane <devin.c.lane@gmail.com>
2022-11-17 12:30:39 -0800

    array chunking part 1

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -5,7 +5,19 @@
 */
 
 function chunkArray(array, size) {
-    // Code goes here
+    const result = []
+    for (const value of array) {
+        // We will think of all our subarrays as the last subarray
+        const lastArray = result[result.length - 1]
+        // is there a last subarray? if not, create one
+        // is the last sub array the size we're looking for? if it is, create a new sub array
+        if (!lastArray || lastArray.length === size) {
+            result.push([value])
+            // if there is a last sub array, push to that value
+            // if the last sub array hasn't hit our desired length yet, push to the sub array
+        } else lastArray.push(value)
+    }
+    return result;
 }
 
 
