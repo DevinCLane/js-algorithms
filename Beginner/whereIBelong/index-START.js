1ba3dbacf4a6cdc25976a8c8a28bd68f6b004c77
Devin Lane <devin.c.lane@gmail.com>
2022-11-28 11:21:39 -0800

    where I belong

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -8,12 +8,31 @@ than 1(index 0), but less than 2(index 1).
 */
 
 
+/* 
+By searching for the value larger than num, we can say that num
+would take that index if it were added to the array
+*/
 
 function whereIBelong(arr, num) {
-   // Code goes here
-
+   arr.sort((a, b) => a - b)
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > num) {
+         // once we find the value greater than num, we return it
+         return i
+      }
+   }
+   // if we don't find a value greater than num, we return the array length (num will go at the end)
+   return arr.length
 }
 
+
+// using indexOf to the find the index of the value directly
+
+function whereIBelong(arr, num) {
+   arr.push(num)
+   arr.sort((a, b) => a - b)
+   return arr.indexOf(num)
+}
 
 
 module.exports = whereIBelong
