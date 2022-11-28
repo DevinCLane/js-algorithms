d5bffb214e426ae6383f574267640b6fdf0cb0d7
Devin Lane <devin.c.lane@gmail.com>
2022-11-28 11:56:11 -0800

    range sum

--- Beginner/rangeSum/index-START.js
+++ Beginner/rangeSum/index-START.js
@@ -1,5 +1,13 @@
+// sum the integers between two numbers
+// rangeSum([1,9]) 
+// Should return 45 i.e 1+2+3+4+5+6+7+8+9
+
 function rangeSum(arr) {
-  // Code goes here
+  let result = 0
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    result += i
+  }
+  return result;
 }
 
 module.exports = rangeSum;
